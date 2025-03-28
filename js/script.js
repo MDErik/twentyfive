26c5982cda28fcb6b26ee1d4c7f8d6f57bb55c94
MDErik <erik.mareniusdehlin@elev.ga.ntig.se>
2025-03-29 00:17:31 +0100

    fredagen 2025-03-28

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@ function showMenu(){
     let layer = document.querySelector(".fade-layer");
     layer.classList.toggle("visible")
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
